70a4a247642b698399ac36545adb72154b121cde
Zane Tung <zanetung13@gmail.com>
2025-11-19 16:28:18 +0000

    datasets

--- web/app/components/datasets/external-api/external-api-modal/Form.tsx
+++ web/app/components/datasets/external-api/external-api-modal/Form.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { RiBookOpenLine } from '@remixicon/react'
 import type { CreateExternalAPIReq, FormSchema } from '../declarations'
 import Input from '@/app/components/base/input'
 import cn from '@/utils/classnames'
-import { useDocLink } from '@/context/i18n'
 
 type FormProps = {
   className?: string
@@ -27,7 +25,6 @@ const Form: FC<FormProps> = React.memo(({
   inputClassName,
 }) => {
   const { t, i18n } = useTranslation()
-  const docLink = useDocLink()
 
   const handleFormChange = (key: string, val: string) => {
     if (key === 'name') {
@@ -55,17 +52,6 @@ const Form: FC<FormProps> = React.memo(({
             {label[i18n.language] || label.en_US}
             {required && <span className='ml-1 text-red-500'>*</span>}
           </label>
-          {variable === 'endpoint' && (
-            <a
-              href={docLink('/guides/knowledge-base/connect-external-knowledge-base') || '/'}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='body-xs-regular flex items-center text-text-accent'
-            >
-              <RiBookOpenLine className='mr-1 h-3 w-3 text-text-accent' />
-              {t('dataset.externalAPIPanelDocumentation')}
-            </a>
-          )}
         </div>
         <Input
           type={type === 'secret' ? 'password' : 'text'}
--- web/app/components/datasets/external-api/external-api-panel/index.tsx
+++ web/app/components/datasets/external-api/external-api-panel/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {
   RiAddLine,
-  RiBookOpenLine,
   RiCloseLine,
 } from '@remixicon/react'
 import { useTranslation } from 'react-i18next'
@@ -52,11 +51,6 @@ const ExternalAPIPanel: React.FC<ExternalAPIPanelProps> = ({ onClose }) => {
           <div className='flex grow flex-col items-start gap-1'>
             <div className='system-xl-semibold self-stretch text-text-primary'>{t('dataset.externalAPIPanelTitle')}</div>
             <div className='body-xs-regular self-stretch text-text-tertiary'>{t('dataset.externalAPIPanelDescription')}</div>
-            <a className='flex cursor-pointer items-center justify-center gap-1 self-stretch'
-              href={docLink('/guides/knowledge-base/connect-external-knowledge-base')} target='_blank'>
-              <RiBookOpenLine className='h-3 w-3 text-text-accent' />
-              <div className='body-xs-regular grow text-text-accent'>{t('dataset.externalAPIPanelDocumentation')}</div>
-            </a>
           </div>
           <div className='flex items-center'>
             <ActionButton onClick={() => onClose()}>
